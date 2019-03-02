2c85b3eda1e74b038c656a4e5772f495240b8766
Alexander <Alexander@DESKTOP-8RM83T5>
2019-03-02 18:23:04 -0300

    Deploying dependencies for unit tests with xUnit and creating the first class and test method.

---
+++ CourseMvc/Test/Test.XUnit/Domain/Entities/CourseTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Test.XUnit.Domain.Entities
+{
+    public class CourseTest
+    {
+        [Fact(DisplayName ="Criar Curso")]
+        public void TestDeveCriarCurso()
+        {
+            //Cenário            
+
+            //Ação
+
+            //Verificação
+        }
+    }
+}
