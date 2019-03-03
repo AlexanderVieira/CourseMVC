a3a7a0ed77538840437d34422ba8dd3a08ff3b93
Alexander <Alexander@DESKTOP-8RM83T5>
2019-03-02 21:42:01 -0300

    Handling invalid parameters template course with exceptions in xUnit.

--- CourseMvc/Test/Test.XUnit/Domain/Entities/CursoTest.cs
+++ CourseMvc/Test/Test.XUnit/Domain/Entities/CursoTest.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using Xunit;
 
 namespace Test.XUnit.Domain.Entities
@@ -8,7 +9,7 @@ namespace Test.XUnit.Domain.Entities
         [Fact(DisplayName ="Criar Curso")]
         public void TestDeveCriarCurso()
         {
-            //Cenario            
+            //########################### Cenario ###########################
             var cursoEsperado = new
             {
                 Nome = "Informática Básica",
@@ -17,16 +18,77 @@ namespace Test.XUnit.Domain.Entities
                 Valor = (double)169
             };
 
-            //Acao
-            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+            //############################ Acao ##############################
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, 
+                cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
 
-            //Verificacao
-            //Assert.Equal(nome, curso.Nome);
-            //Assert.Equal(cargaHoraria, curso.CargaHoraria);
-            //Assert.Equal(publicoAlvo, curso.PublicoAlvo);
-            //Assert.Equal(valor, curso.Valor);
-
+            //######################### Verificacao ##########################
             cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void TestCursoNaoDeveTerNomeInvalido(string nomeInvalido)
+        {
+            //########################### Cenario ###########################
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)40,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)169
+            };
+
+            //######################### Verificacao ##########################
+            var message = Assert.Throws<ArgumentException>(() =>
+                new Curso(nomeInvalido, cursoEsperado.CargaHoraria, 
+                    cursoEsperado.PublicoAlvo, cursoEsperado.Valor)).Message;
+            Assert.Equal("Nome Invalido!", message);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void TestCursoNaoDeveTerCargaHorariaMenorQue1(double cargaHorariaInvalida)
+        {
+            //########################### Cenario ###########################
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)40,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)169
+            };
+
+            //######################### Verificacao ##########################
+            var message = Assert.Throws<ArgumentException>(() =>
+                new Curso(cursoEsperado.Nome, cargaHorariaInvalida, 
+                    cursoEsperado.PublicoAlvo, cursoEsperado.Valor)).Message;
+            Assert.Equal("Carga horaria invalida!", message);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void TestCursoNaoDeveTerValorMenorQue1(double valorInvalido)
+        {
+            //########################### Cenario ###########################
+            var cursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)40,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)169
+            };
+
+            //######################### Verificacao ##########################
+            var message = Assert.Throws<ArgumentException>(() =>
+                new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, 
+                    cursoEsperado.PublicoAlvo, valorInvalido)).Message;
+            Assert.Equal("Valor invalido!", message);
         }
     }
 
@@ -42,6 +104,21 @@ namespace Test.XUnit.Domain.Entities
     {
         public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
         {
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentException("Nome Invalido!");
+            }
+
+            if (cargaHoraria < 1)
+            {
+                throw new ArgumentException("Carga horaria invalida!");
+            }
+
+            if (valor < 1)
+            {
+                throw new ArgumentException("Valor invalido!");
+            }
+
             Nome = nome;
             CargaHoraria = cargaHoraria;
             PublicoAlvo = publicoAlvo;
